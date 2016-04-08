dc8dee08462b7e91086dcddd076523d8a1d89a2c
smartcoach <service.wechat@smartcoach.cn>
2016-04-08 15:19:36 +0800

    Create content.js

---
+++ content.js
@@ -0,0 +1,24 @@
+var topBar={
+    src:"./1.jpg",
+    style:"width:100vw;max-height:30vh;",
+};
+var data=[
+    {
+        name:"中文",
+        items:[
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'测试'},
+        ]
+    },
+    {
+        name:"传统",
+        items:[
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'传统'},
+        ]
+    },
+    {
+        name:"文化",
+        items:[
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'文化'},
+        ],
+    },
+];
