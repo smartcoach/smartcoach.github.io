a9a72679ab50aa179a6793a34638be9fa0daf654
smartcoach <service.wechat@smartcoach.cn>
2016-04-08 15:02:14 +0800

    Added files via upload

---
+++ test.js
@@ -0,0 +1,36 @@
+var topBar={
+    src:"./1.jpg",
+    style:"width:100vw;max-height:30vh;",
+};
+var data=[
+    {
+        name:"中文",
+        items:[
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'hhh1'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'hhh2'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'hhh2'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'hhh2'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'hhh2'},
+        ]
+    },
+    {
+        name:"传统",
+        items:[
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'传统'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'传统'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'传统'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'传统'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'传统'},
+        ]
+    },
+    {
+        name:"文化",
+        items:[
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'文化'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'文化'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'文化'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'文化'},
+            {url:"http://www.qq.com",img:"https://www.baidu.com/img/bd_logo1.png",tip:'文化'},
+        ],
+    },
+];
